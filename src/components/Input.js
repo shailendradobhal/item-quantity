1680d4ceefa7e3177415ed27cc808cc1ae1dafff
Shailendra Dobhal <sdobhal@sapient.com>
2018-09-10 20:10:52 +0530

    master

--- src/components/Input.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Input({ amount, label, onChange }) {
-  return (
-    <input type="text" className="form-control text-center" aria-labelledby={label} value={amount} onChange={onChange} />
-  );
-}
-
-export default Input;
